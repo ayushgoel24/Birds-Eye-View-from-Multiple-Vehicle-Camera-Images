e2471b9a76345895dd682df18a1a4b420e6365cf
Ayush Goel <ayush.goel2427@gmail.com>
2023-12-11 23:23:15 -0500

    removed cache and configurations from package

--- multicam_birdseye/cache/training_configuration_manager.py
+++
@@ -1,8 +0,0 @@
-class ConfigurationManager:
-
-    _instance = None
-
-    def __new__(cls, *args, **kwargs):
-        if cls._instance is None:
-            cls._instance = super(ConfigurationManager, cls).__new__(cls, *args, **kwargs)
-        return cls._instance
